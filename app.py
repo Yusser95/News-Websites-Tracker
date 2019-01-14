6c67ca1a7507b8643245048c8804de65e844491c
MAC <mac@MACs-MacBook-Pro.local>
2019-01-14 05:58:42 +0200

    V1

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def main():
 		resp = {"success":False}
 		selected_domains = request.form.get('domains')
 		selected_keywords = request.form.get('keywors')
+
+		print("\n selected_domains : ",selected_domains,"\n selected_keywords : ",selected_keywords)
 		try:
 			data = get_data(selected_domains,selected_keywords)
 			resp["data"] = data
@@ -44,17 +46,21 @@ def main():
 		except Exception as e:
 			print(e)
 
-		
+
+
 		resp = json.dumps(resp, ensure_ascii=False).encode('utf8')
-		resp = make_response(resp);
-        resp.headers["Content-Type"]='application/json; charset=utf-8';
+		resp = make_response(resp)
+		resp.headers["Content-Type"]='application/json; charset=utf-8'
 
 		return resp
 	else: # get request
 		domains = get_domains()
 		keywords = getkeywords()
 		data = get_data()
-		return render_template('index.html',keywords = keywords ,domains = domains, data=data)
+		colors = ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"]
+		labels = [d.url for d in domains]
+		domains_keywords_count = [len(Pargraph.query.filter_by(domain_id=d.id).all()) for d in domains]
+		return render_template('index.html',keywords = keywords ,domains = domains, data=data ,labels=labels,colors=colors, domains_keywords_count=domains_keywords_count)
  
 
 
@@ -68,21 +74,23 @@ def getkeywords():
 	return keywords
 
 def get_data(selected_domains=None , selected_keywords=None):
-	# ss= Sentiment.query.filter_by(topic_id=selected_topic[2:]).order_by(Sentiment.date).all()
+	# ss= Sentiment.query.filter_by(keyword_id=selected_keyword[2:]).order_by(Sentiment.date).all()
 	# ss= Sentiment.query.filter(Sentiment.tweet_id.in_(selected_tweets)).order_by(Sentiment.date).all()
 	pargraphs = []
-	if selected_domains and selected_keywords:
+	if selected_domains is not None and selected_keywords is not None:
 		p_ids = Keyword_Pargraph.query.filter(Keyword_Pargraph.keyword_id.in_(selected_keywords)).all()
 		p_ids = [p.pargraph_id for p in p_ids]
 		pargraphs = Pargraph.query.filter(Pargraph.domain_id.in_(selected_domains),Pargraph.id.in_(p_ids)).all()
-	elif selected_domains:
+	elif selected_domains is not None:
 		pargraphs = Pargraph.query.filter(Pargraph.domain_id.in_(selected_domains)).all()
-	elif selected_keywords:
+	elif selected_keywords is not None:
 		p_ids = Keyword_Pargraph.query.filter(Keyword_Pargraph.keyword_id.in_(selected_keywords)).all()
 		p_ids = [p.pargraph_id for p in p_ids]
 		pargraphs = Pargraph.query.filter(Pargraph.id.in_(p_ids)).all()
 	else:
-		pargraphs = Pargraph.query.all()
+		p_ids = Keyword_Pargraph.query.all()
+		p_ids = [p.pargraph_id for p in p_ids]
+		pargraphs = Pargraph.query.filter(Pargraph.id.in_(p_ids)).all()
 
 
 	data = [{"html":p.html , "domain":Domain.query.filter_by(id=p.domain_id).one().url ,"order":len(Keyword_Pargraph.query.filter_by(pargraph_id=p.id).all())} for p in pargraphs]
@@ -105,54 +113,53 @@ def admin():
 
 
 
-###########      topic routes
+###########      keyword routes
 
 
 
 
 
-@app.route("/admin/topic/show" , methods =["GET"])
-def showtopic():
-	# topics = [{'id':1,'name':'sport' , 'words':'("a","b")'}]
-	tt= Topic.query.all()
-	topics = [{'id':t.id , 'name':t.name , 'words':t.words} for t in tt]
-	return render_template('admin/topic/show.html',topics=topics)
+@app.route("/admin/keyword/show" , methods =["GET"])
+def showkeyword():
 
-@app.route("/admin/topic/delete/<id>" , methods =["GET"])
-def deletetopic(id):
+	keywords = Keyword.query.all()
+	return render_template('admin/keyword/show.html',keywords=keywords)
+
+@app.route("/admin/keyword/delete/<id>" , methods =["GET"])
+def deletekeyword(id):
 	print("deleted " , id)
-	return redirect('/admin/topic/show')
+	return redirect('/admin/keyword/show')
 
-@app.route("/admin/topic/edit/<id>" , methods =["GET" , "POST"])
-def edittopic(id):
+@app.route("/admin/keyword/edit/<id>" , methods =["GET" , "POST"])
+def editkeyword(id):
 	print(id)
 	# edit
 	if request.method == "POST":
 		name = request.form.get('name')
 		words = request.form.get('words')
 		print(name,words)
-		return redirect('/admin/topic/show')
+		return redirect('/admin/keyword/show')
 	# show  one row
 	elif request.method == "GET":
-		# topic = {'id':1,'name':'sport' , 'words':'("a","b")'}
+		# keyword = {'id':1,'name':'sport' , 'words':'("a","b")'}
 
-		t= Topic.query.filter_by(id=id).first()
-		topic = {'id':t.id , 'name':t.name , 'words':t.words}
-		return render_template('/admin/topic/edit.html',item = topic)
+		# t= keyword.query.filter_by(id=id).first()
+		keyword = Keyword.query.filter_by(id=id).one()
+		return render_template('/admin/keyword/edit.html',item = keyword)
 	return "404"
 
 
-@app.route("/admin/topic/create" , methods =["GET" , "POST"])
-def createtopic():
+@app.route("/admin/keyword/create" , methods =["GET" , "POST"])
+def createkeyword():
 	# edit
 	if request.method == "POST":
 		name = request.form.get('name')
 		words = request.form.get('words')
 		print(name,words)
-		return redirect('/admin/topic/show')
+		return redirect('/admin/keyword/show')
 	# show  one row
 	elif request.method == "GET":
-		return render_template('/admin/topic/create.html')
+		return render_template('/admin/keyword/create.html')
 	return "404"
 
 
@@ -161,49 +168,49 @@ def createtopic():
 
 
 
-###########      user routes
+###########      domain routes
 
 
 
 
 
-@app.route("/admin/user/show" , methods =["GET"])
-def showuser():
-	users = [{'id':1,'name':'sport' , 'words':'("a","b")'}]
-	return render_template('admin/user/show.html',users=users)
+@app.route("/admin/domain/show" , methods =["GET"])
+def showdomain():
+	domains = Domain.query.all()
+	return render_template('admin/domain/show.html',domains=domains)
 
-@app.route("/admin/user/delete/<id>" , methods =["GET"])
-def deleteuser(id):
+@app.route("/admin/domain/delete/<id>" , methods =["GET"])
+def deletedomain(id):
 	print("deleted " , id)
-	return redirect('/admin/user/show')
+	return redirect('/admin/domain/show')
 
-@app.route("/admin/user/edit/<id>" , methods =["GET" , "POST"])
-def edituser(id):
+@app.route("/admin/domain/edit/<id>" , methods =["GET" , "POST"])
+def editdomain(id):
 	print(id)
 	# edit
 	if request.method == "POST":
 		name = request.form.get('name')
 		words = request.form.get('words')
 		print(name,words)
-		return redirect('/admin/user/show')
+		return redirect('/admin/domain/show')
 	# show  one row
 	elif request.method == "GET":
-		user = {'id':1,'name':'sport' , 'words':'("a","b")'}
-		return render_template('/admin/user/edit.html',item = user)
+		domain = Domain.query.filter_by(id=id).one()
+		return render_template('/admin/domain/edit.html',item = domain)
 	return "404"
 
 
-@app.route("/admin/user/create" , methods =["GET" , "POST"])
-def createuser():
+@app.route("/admin/domain/create" , methods =["GET" , "POST"])
+def createdomain():
 	# edit
 	if request.method == "POST":
 		name = request.form.get('name')
 		words = request.form.get('words')
 		print(name,words)
-		return redirect('/admin/user/show')
+		return redirect('/admin/domain/show')
 	# show  one row
 	elif request.method == "GET":
-		return render_template('/admin/user/create.html')
+		return render_template('/admin/domain/create.html')
 	return "404"
 
 
